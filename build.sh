1d30b3715b20d466e7aaf0624e16be46f226d358
Vo Minh Thu <thu@hypered.io>
2017-04-08 20:43:25 +0200

    Support GHC 7.10.3.

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
 #! /usr/bin/env bash
 
+STACK_IMAGE=${1:-7.8.4}
+
 docker run \
   -v `pwd`/../minicron:/home/gusdev/minicron \
   -v `pwd`/../humming:/home/gusdev/humming \
   -v `pwd`:/home/gusdev/hurling \
-  images.reesd.com/reesd/stack:7.8.4 \
+  images.reesd.com/reesd/stack:$STACK_IMAGE \
   cabal install minicron/minicron.cabal humming/humming.cabal hurling/hurling.cabal
